67e1bb135a2a2691dbbfa2e3ef62b65cb783f068
Edoardo Vincenzi <85701416+edoardovincenzi@users.noreply.github.com>
2023-05-31 14:30:22 +0200

    Update CardProject.tsx, changing fontsize for title cards

--- src/app/[lng]/components/cardproject/CardProject.tsx
+++ src/app/[lng]/components/cardproject/CardProject.tsx
@@ -22,7 +22,7 @@ const CardProject = ({
       </div>
       <div className="pb-2 relative py-10 px-4 w-full grid grid-cols-1 grid-rows-2 h-full justify-center items-start">
         <div>
-          <h2 className="block antialiased tracking-normal text-4xl leading-[1.3] text-textCardDescription mb-6 font-bold ">
+          <h2 className="block antialiased tracking-normal text-3xl leading-[1.3] text-textCardDescription mb-6 font-bold ">
             {title.toUpperCase()}
           </h2>
           <p className="text-sm md:text-base row-span-3  text-textCardDescription">
