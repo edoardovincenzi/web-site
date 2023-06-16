0fa6167174096d12edefcc162a71f628c2755d54
Edoardo Vincenzi <85701416+edoardovincenzi@users.noreply.github.com>
2023-06-16 09:58:31 +0200

    Update link pdf CV

--- src/costant.ts
+++ src/costant.ts
@@ -1,5 +1,5 @@
 export const SHARE_DOWNLOAD_CV =
-  'https://drive.google.com/file/d/1yxqbE7y442ySHDADp9psvjIfRTS69Pzn/view?usp=sharing';
+  'https://drive.google.com/file/d/1NXJByqKQRzkK7QwTonc0eKG09vhdq8jf/view?usp=sharing';
 export const LINK_GITHUB = 'https://github.com/edoardovincenzi';
 export const LINK_LINKLEDIN = 'https://www.linkedin.com/in/edoardo-vincenzi/';
 export const EMAIL = 'edoardo.vincenzi95@gmail.com';
