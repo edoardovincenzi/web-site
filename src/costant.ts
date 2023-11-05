75ff3ba533b29329f94cc2d12d8c2097d51fd2e3
Edoardo Vincenzi <85701416+edoardovincenzi@users.noreply.github.com>
2023-11-05 10:56:42 -0500

    Update costant.ts

--- src/costant.ts
+++ src/costant.ts
@@ -1,5 +1,5 @@
 export const SHARE_DOWNLOAD_CV =
-  'https://drive.google.com/file/d/1sCg_hWmLZPro5_b3GL8PbnxPkjwSCziN/view?usp=drivesdk';
+  'https://drive.google.com/file/d/1kxra6KUGFXbzgFx_Kx2jaZYWc9At0EsT/view?usp=drivesdk';
 export const LINK_GITHUB = 'https://github.com/edoardovincenzi';
 export const LINK_LINKLEDIN = 'https://www.linkedin.com/in/edoardo-vincenzi/';
 export const EMAIL = 'edoardo.vincenzi95@gmail.com';
