21c69cb535c148dd868a79cbb53ff7c7e2c4c9be
edoardovincenzi <edoardo.vincenzi95@gmail.com>
2024-03-01 14:54:42 -0500

    remove projets section and change description text in all language

--- src/app/[lng]/components/Menu.tsx
+++ src/app/[lng]/components/Menu.tsx
@@ -8,7 +8,7 @@ import { Trans } from 'react-i18next';
 const Menu = ({ t, lng }: any) => {
   return (
     <div className="flex max-md:flex-col w-full h-[80%] mb-4 gap-3">
-      <div className={`transition-all w-full md:w-1/2 h-4/6 md:h-full `}>
+      <div className={`transition-all w-full md:w-2/3 h-4/6 md:h-full `}>
         <div className="flex text-textCardDescription flex-col justify-center items-center h-full px-6 pt-4 pb-6 bg-bgCardDescription rounded-3xl gap-y-2 lg:gap-y-6 relative">
           <h3 className="title-card-home">
             {t('description_card.title', 'Description')}
@@ -24,8 +24,8 @@ const Menu = ({ t, lng }: any) => {
           </p>
         </div>
       </div>
-      <div className="h-2/6 flex flex-col w-full md:h-full md:w-1/2 gap-3">
-        <Link
+      <div className="h-2/6 flex flex-col w-full md:h-full md:w-1/3 gap-3">
+        {/* <Link
           href={`/${lng}/projects`}
           replace
           className="card-small-home bg-bgCard1"
@@ -34,7 +34,7 @@ const Menu = ({ t, lng }: any) => {
           <h3 className="title-card-home">
             {t('projects_card.title', 'Projects')}
           </h3>
-        </Link>
+        </Link> */}
         <Link
           href={`/${lng}/resume`}
           replace
@@ -52,7 +52,4 @@ const Menu = ({ t, lng }: any) => {
 
 export default Menu;
 
-const descriptionDefaultText = `I am a passionate FRONT-END programmer with solid experience in designing and developing various types of web projects. I am motivated to create extraordinary user experiences using the latest technologies and innovative designs.
-My passion for running has taught me the importance of perseverance and discipline, which I apply to my work as well.
-I enjoy collaborating with other industry professionals to create high-quality digital products that meet the needs of clients.
-`;
+const descriptionDefaultText = `I am a <0>software developer</0> with almost 5 years of experience, having worked in both front-end and back-end. This experience has allowed me to gain a comprehensive understanding of software development. <1></1> <1></1>I strongly believe that investing in the <0>fundamentals of programming</0>, <0>algorithms</0>, and <0>design patterns</0> is crucial to becoming a better developer. <1></1> <1></1>Exploring and experimenting with new technologies is always a valuable learning opportunity. Having self-confidence and maintaining a critical eye can be challenging, but it leads to greater openness and allows for constant growth and improvement.`;
